c3968c4e46a92837fd5f968c5702ba53f80466c2
Boycov Andrey <boec.n1@mail.ru>
2020-01-05 01:00:25 +0300

    demo portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ChangeLogFormComponent} from './change-log-form/change-log-form.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'My Portfolio';
+  constructor( public dialog: MatDialog ) {}
+
+  openExample(component) {
+    this.dialog.open(
+      ChangeLogFormComponent
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import {ChangeLogFormComponent} from './change-log-form/change-log-form.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangeLogFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule
   ],
+  entryComponents: [ChangeLogFormComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
